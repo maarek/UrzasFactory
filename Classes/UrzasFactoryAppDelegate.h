62174ff44896570c9a27db899b56feb460403a98
maarek <luminare@gmail.com>
2010-02-20 01:57:12 -0600

    initial commit

---
+++ Classes/UrzasFactoryAppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  UrzasFactoryAppDelegate.h
+//  UrzasFactory
+//
+//  Created by Jeremy Lyman on 2/20/10.
+//  Copyright Lost Creatures 2010. All rights reserved.
+//
+
+@interface UrzasFactoryAppDelegate : NSObject <UIApplicationDelegate> {
+
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UrzasFactory
+//
+//  Created by Jeremy Lyman on 2/20/10.
+//  Copyright Lost Creatures 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
